9a74dd677882f749372e822f6a94a7ff6f3a08c0
Maxime Princelle <maxime@princelle.org>
2022-02-15 16:07:06 +0100

    Initial Commit

---
+++ NewDriver/NewDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewDriverApp.swift
+//  NewDriver
+//
+//  Created by Maxime Princelle on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
